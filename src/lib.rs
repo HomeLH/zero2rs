b04c7d1e9ee6de9817c166a7ee4e1f849c79aa5e
hogan <li_hao@outlook.com>
2023-03-02 23:03:41 +0800

    add subscription route

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 use actix_web::{web, App, HttpServer, HttpRequest, Responder, HttpResponse, dev::Server};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize, Debug)]
+struct FormData {
+    email: String,
+    name: String
+}
+
 async fn greet(req: HttpRequest) -> impl Responder{
     let name = req.match_info().get("name").unwrap_or("");
     format!("Hello {}!", name)
 }
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
+async fn health_check(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// async fn subscribe(_req: HttpRequest) -> HttpResponse {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    println!("{:?}", _form);
     HttpResponse::Ok().finish()
 }
 
@@ -17,6 +29,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         .route("/", web::get().to(greet))
         // .route("/{name}", web::get().to(greet))
         .route("/healthcheck", web::get().to(health_check))
+        .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
