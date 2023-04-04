332521746b2fbac34f9705284604ce540bf8a1d4
lihao3651 <lihao3651@crscd.com.cn>
2023-04-04 18:09:45 +0800

    tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,15 @@
 use std::net::TcpListener;
-use actix_web::{HttpServer, dev::Server, App, web, middleware::Logger};
+use actix_web::{HttpServer, dev::Server, App, web};
 use sqlx::PgPool;
 use crate::router::health_check;
 use crate::router::subscribe;
+use tracing_actix_web::TracingLogger;
 // Note: think deeply
 pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error> {
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
-        .wrap(Logger::default())
+        .wrap(TracingLogger::default())
         // .route("/{name}", web::get().to(greet))
         .route("/healthcheck", web::get().to(health_check))
         .route("/subscriptions", web::post().to(subscribe))
