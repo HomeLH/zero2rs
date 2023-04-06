48392cfbf3057a0f42865c6949f0213361e4dad9
hogan <li_hao@outlook.com>
2023-04-06 20:47:58 +0800

    add waiting postgres

--- scripts/init_db_client.sh
+++ scripts/init_db_client.sh
@@ -5,10 +5,10 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 HOST="127.0.0.1"
 
 export PGPASSWORD="${DB_PASSWORD}"
-#until psql -h "${HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-#  >&2 echo "Postgres is still unavailable - sleeping"
-#    sleep 1
-#done
+until psql -h "${HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${HOST}:${DB_PORT}/${DB_NAME}
 echo ${DATABASE_URL}
