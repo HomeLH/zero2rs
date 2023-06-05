3baf14383f50db97192b254dbd52eb5dedab1d7c
lihao3651 <lihao3651@crscd.com.cn>
2023-06-05 19:01:40 +0800

    add email client restful test

--- src/email_client.rs
+++ src/email_client.rs
@@ -20,7 +20,58 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        self.http_client
+            .post(&format!("{}/email", self.base_url))
+            .json(&SendEmailRequest {
+                from: self.sender.as_ref(),
+                to: recipient.as_ref(),
+                subject,
+                html_content,
+                text_content,
+            })
+            .send()
+            .await
+            .map_err(|_| "failed to send email".to_string())?;
+        Ok(())
     }
-    
+}
+// implement SendEmailRequest structure
+#[derive(serde::Serialize)]
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_content: &'a str,
+    text_content: &'a str,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::faker::name::en::Name;
+    use fake::Fake;
+    use wiremock::matchers::{any, header, header_exists, method, path};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject = Sentence(1..2).fake::<String>();
+        let content = Paragraph(1..10).fake::<String>();
+
+        //act 
+        let _ = email_client.send_email(subscriber_email, &subject, &content, &content).await;
+    }
 }
